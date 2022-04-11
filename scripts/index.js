4fc6f1843142648166fb1215f2676324f84bf2f4
Dmitriy Mysyagin <dmitry.mysyagin@yandex.ru>
2022-04-11 19:24:19 +0300

    feat: Добавлен скрипт на добавление старотовых карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
 const editButton = document.querySelector('.profile__edit');
 const popupPlaceProfile = document.querySelector('.popup_place_profile');
 const popupForm = document.querySelector('.popup__form');
@@ -6,10 +33,35 @@ const profileName = document.querySelector('.profile__name');
 const profileActivity = document.querySelector('.profile__activity');
 const popupName = document.querySelector('.popup__input_place_name');
 const popupActivity = document.querySelector('.popup__input_place_activity');
+const popupCardName = document.querySelector('.popup__input_place_card-name');
+const popupCardLink = document.querySelector('.popup__input_place_card-link');
 
 const profileAddButton = document.querySelector('.profile__add-button');
 const popupPlaceCard = document.querySelector('.popup_place_card'); 
-const popupCrossPlaceCard = popupPlaceCard.querySelector('.popup__cross'); 
+const popupCrossPlaceCard = popupPlaceCard.querySelector('.popup__cross');
+const popupSubmitPlaceCard = popupPlaceCard.querySelector('.popup__submit');
+
+const elementsList = document.querySelector('.elements__list');
+const template = document.querySelector('.template');
+
+function render() {
+  const html = initialCards.map(getElement);
+  elementsList.append(...html);
+}
+
+function getElement(item) {
+  const newItem = template.content.cloneNode(true);
+  const elementsTitle = newItem.querySelector('.elements__title');
+  const elementsImage = newItem.querySelector('.elements__image');
+  elementsTitle.textContent = item.name; 
+  elementsImage.setAttribute('src',`${item.link}`);
+  elementsImage.setAttribute('alt',`${item.name}`);
+  return newItem;
+}
+
+render()
+
+
 
 
 function openPopup(modalWindow) {
@@ -28,13 +80,14 @@ function formSubmitHandler(evt) {
 }
 
 editButton.addEventListener('click', function() {
-  popupPlaceProfile.classList.add('popup_open');
-  if (popupPlaceProfile.classList.contains('popup_open')) {
+  if (!popupPlaceProfile.classList.contains('popup_open')) {
     popupName.value = profileName.textContent;
     popupActivity.value = profileActivity.textContent;
-}});
-
+}
+  openPopup(popupPlaceProfile);
+});
 popupCrossPlaceProfile.addEventListener('click', () => closePopup(popupPlaceProfile));
+
 profileAddButton.addEventListener('click', () => openPopup(popupPlaceCard));
 popupCrossPlaceCard.addEventListener('click', () => closePopup(popupPlaceCard));
 
