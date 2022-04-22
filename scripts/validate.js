2f9aba904e103b015e89a680f355b9bb7cbddb45
Dmitriy Mysyagin <dmitry.mysyagin@yandex.ru>
2022-04-22 11:57:19 +0300

    feat: добавлены html атрибуты input в profile

---
+++ scripts/validate.js
@@ -0,0 +1,21 @@
+
+const showInputError = (formElement, inputElement, inputErrorClass, errorClass, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(`${inputErrorClass}`);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(`${errorClass}`);
+  return console.log(formElement);
+};
+
+const enableValidation = function ({formElement, inputElement, inputErrorClass, errorClass, inputSelector}) {
+  showInputError(formElement, inputElement, inputErrorClass, errorClass, inputSelector.validationMessage);
+};
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}); 
