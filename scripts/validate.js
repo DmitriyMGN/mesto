1a70807682d81c2a8c1373288c53fbb52d871d8a
Dmitriy Mysyagin <dmitry.mysyagin@yandex.ru>
2022-04-24 19:21:59 +0300

    feat:Первичный скрипт

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,15 +1,37 @@
-
-const showInputError = (formElement, inputElement, inputErrorClass, errorClass, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(`${inputErrorClass}`);
+const showInputError = (form,input,inputError,spanError,errorMessage) => {
+  const errorForm = document.querySelector(`${form}`);
+  const inputElement = document.querySelector(`${input}`);
+  const errorElement = errorForm.querySelector('.popup__error');
+  console.log(errorElement);
+  inputElement.classList.add(`${inputError}`);
+  errorElement.classList.add(`${spanError}`);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add(`${errorClass}`);
-  return console.log(formElement);
 };
 
-const enableValidation = function ({formElement, inputElement, inputErrorClass, errorClass, inputSelector}) {
-  showInputError(formElement, inputElement, inputErrorClass, errorClass, inputSelector.validationMessage) ;
+const hideInputError = (formElement, inputElement, inputError, spanError) => {
+  const errorFormElement = document.querySelector(`${formElement}`);
+  const errorElement = errorFormElement.querySelector(`${inputElement}-error`);
+  inputElement.classList.add(`${inputError}`);
+  errorElement.classList.add(`${spanError}`);
+  errorElement.textContent = '';
 };
+
+const checkInputValidity = function (form,input,inputError,spanError) {
+  const validateInput = document.querySelector(`${input}`);
+  const errorMessage = validateInput.validationMessage;
+  console.log(validateInput);
+  if(!validateInput.validity.valid) {
+    console.log('Не валидно');
+    showInputError(form,input,inputError,spanError,errorMessage);
+  } else {
+    hideInputError(form,input,inputError,spanError);
+    console.log('Валидно');
+  }
+};
+
+const enableValidation = ({formSelector,inputSelector,inputErrorClass,errorClass}) => {
+  checkInputValidity(formSelector,inputSelector,inputErrorClass,errorClass);
+}
 
 enableValidation({
   formSelector: '.popup__form',
@@ -19,3 +41,4 @@ enableValidation({
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 }); 
+
