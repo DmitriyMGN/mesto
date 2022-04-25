0ec71bd3ceee704a6bae621d23e6116349895c86
Dmitriy Mysyagin <dmitry.mysyagin@yandex.ru>
2022-04-25 18:44:17 +0300

    feat:Реализована первичная  валидация

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,48 +1,42 @@
-const showInputError = (form,input,inputError,spanError,errorMessage) => {
-  const errorForm = document.querySelector(`${form}`);
-  const inputElement = document.querySelector(`${input}`);
-  const errorElement = errorForm.querySelector('.popup__error');
-  inputElement.classList.add(`${inputError}`);
-  errorElement.classList.add(`${spanError}`);
-  errorElement.textContent = errorMessage;
+const showInputError = (inputErrorClass, errorClass, formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.classList.add(errorClass);
+  errorElement.textContent = inputElement.validationMessage;
 };
 
-const hideInputError = (form, input, inputError,spanError) => {
-  const errorForm = document.querySelector(`${form}`);
-  const inputElement = document.querySelector(`${input}`);
-  const errorElement = errorForm.querySelector('.popup__error');
-  inputElement.classList.remove(`${inputError}`);
-  errorElement.classList.remove(`${spanError}`);
-  console.log(`${spanError}`)
+const hideInputError = (inputErrorClass, errorClass, formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = function (form,input,inputError,spanError) {
-  const validateInput = document.querySelector(`${input}`);
-  const errorMessage = validateInput.validationMessage;
-  if(!validateInput.validity.valid) {
+const checkInputValidity = function (object, formElement, inputElement) {
+  if(!inputElement.validity.valid) {
     console.log('Не валидно');
-    showInputError(form,input,inputError,spanError,errorMessage);
+    showInputError(object.inputErrorClass, object.errorClass, formElement, inputElement);
   } else {
-    hideInputError(form,input,inputError,spanError);
+    hideInputError(object.inputErrorClass, object.errorClass, formElement, inputElement);
     console.log('Валидно');
   }
 };
 
-const setEventListeners = (form, input) => {
-  const inputList = Array.from(document.querySelectorAll(`${input}`));
-  inputList.forEach((input1) => {
-    input1.addEventListener('input', function () {
-      checkInputValidity(form,input);
-      console.log(input);
+const setEventListeners = (object, formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(`${object.inputSelector}`));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(object, formElement, inputElement);
     });
   });
 };
 
-const enableValidation = ({formSelector,inputSelector,inputErrorClass,errorClass}) => {
-  setEventListeners(formSelector,inputSelector);
-  checkInputValidity(formSelector,inputSelector,inputErrorClass,errorClass);
-
+const enableValidation = (object) => {
+  const formList = Array.from(document.querySelectorAll(object.formSelector));
+  console.log(object.formSelector);
+  formList.forEach((formElement) => {
+    setEventListeners(object, formElement);
+  });
 }
 
 enableValidation({
@@ -53,4 +47,53 @@ enableValidation({
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 }); 
+
+// const showInputError = (form,input,inputError,spanError,errorMessage) => {
+//   const errorForm = document.querySelector(`${form}`);
+//   const inputElement = document.querySelector(`${input}`);
+//   const errorElement = errorForm.querySelector('.popup__error');
+//   inputElement.classList.add(`${inputError}`);
+//   errorElement.classList.add(`${spanError}`);
+//   errorElement.textContent = errorMessage;
+// };
+
+// const hideInputError = (form, input, inputError,spanError) => {
+//   const errorForm = document.querySelector(`${form}`);
+//   const inputElement = document.querySelector(`${input}`);
+//   const errorElement = errorForm.querySelector('.popup__error');
+//   inputElement.classList.remove(`${inputError}`);
+//   errorElement.classList.remove(`${spanError}`);
+//   console.log(`${spanError}`)
+//   errorElement.textContent = '';
+// };
+
+// const checkInputValidity = function (form,input,inputError,spanError) {
+//   const validateInput = document.querySelector(`${input}`);
+//   const errorMessage = validateInput.validationMessage;
+//   if(!validateInput.validity.valid) {
+//     console.log('Не валидно');
+//     showInputError(form,input,inputError,spanError,errorMessage);
+//   } else {
+//     hideInputError(form,input,inputError,spanError);
+//     console.log('Валидно');
+//   }
+// };
+
+// const setEventListeners = (form, input) => {
+//   const inputList = Array.from(document.querySelectorAll(`${input}`));
+//   inputList.forEach((input1) => {
+//     input1.addEventListener('input', function () {
+//       checkInputValidity(form,input);
+//       console.log(input);
+//     });
+//   });
+// };
+
+// const enableValidation = ({formSelector,inputSelector,inputErrorClass,errorClass}) => {
+//   setEventListeners(formSelector,inputSelector);
+//   checkInputValidity(formSelector,inputSelector,inputErrorClass,errorClass);
+
+// }
+
+
 
